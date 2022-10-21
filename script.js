719c9b8507a438c998bd24c2c894eabc563c03c5
EvtimEvtimov <8542031+evtimov-ptr@users.noreply.github.com>
2022-10-21 12:15:53 +0300

    Refactored code

--- script.js
+++ script.js
@@ -22,16 +22,23 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 const log = function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // When there is no input
+
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    // document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number! ';
+    displayMessage('🎉 Correct Number!');
+    // document.querySelector('.message').textContent = '🎉 Correct Number! ';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -45,12 +52,14 @@ const log = function () {
     // When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? '📈 Too high!' : '📈 Too low!';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Too high!' : '📈 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📈 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
+      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -82,7 +91,10 @@ const restart = function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
+
+  displayMessage('Start guessing...');
+  // document.querySelector('.message').textContent = 'Start guessing...';
+
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
